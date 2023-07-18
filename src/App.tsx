a65bec3b35eb0f35434a5eec4965968f46fd6a37
Damir Drempetic <damir.drempetic@p-tri.com>
2023-07-18 16:23:44 +0200

    Implement mapbox gl draw

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import maplibregl from "maplibre-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
-import Map, { MapRef } from "react-map-gl";
-import "./index.css";
+import { useMemo, useRef } from "react";
 
-import { useRef } from "react";
+import MapboxDraw, {
+  DrawCreateEvent,
+  DrawUpdateEvent,
+} from "@mapbox/mapbox-gl-draw";
+import maplibregl from "maplibre-gl";
+import Map, { MapRef } from "react-map-gl";
+
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import "maplibre-gl/dist/maplibre-gl.css";
+import "./index.css";
 
 export default function App() {
   const mapRef = useRef<MapRef>(null);
+
+  const mapboxDraw = useMemo(() => new MapboxDraw(), []);
+
+  const handleOnDrawControlCreate = (event: DrawCreateEvent) => {
+    console.log(event);
+  };
+
+  const handleOnDrawControlUpdate = (event: DrawUpdateEvent) => {
+    console.log(event);
+  };
 
   return (
     <Map
@@ -14,6 +30,14 @@ export default function App() {
       style={{ height: "100vh" }}
       mapLib={maplibregl}
       mapStyle="https://api.maptiler.com/maps/streets-v2/style.json?key=LiH20XNxcFiTXyT4fgjM"
+      onLoad={() => {
+        mapRef.current?.addControl(mapboxDraw);
+        const map = mapRef.current?.getMap();
+        if (map) {
+          map.on("draw.create", handleOnDrawControlCreate);
+          map.on("draw.update", handleOnDrawControlUpdate);
+        }
+      }}
     />
   );
 }
